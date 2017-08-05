4fbba7c47596eb3c0df846eb74110992abe4bdc3
dragosstudio <dragosstudio@gmail.com>
2017-08-05 18:19:34 +0300

    asd

--- com.sci/src/com/sci/Main.java
+++ com.sci/src/com/sci/Main.java
@@ -1,14 +1,20 @@
 package com.sci;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduceti numarul: ");
-        int number = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Introduceti numarul: ");
+//        int number = sc.nextInt();
+
+        Circle c1=new Circle();
+        c1.setRadius(30.0);
+        System.out.println(c1.radius);
+        System.out.println(c1.getAria());
     }
 }
+
 
 
